8a63f81e52ee85c0edc43f4907097a78ff9cba9c
S-Jaswal <sjasdocs@gmail.com>
2023-12-07 13:41:01 -0500

    Final Version of the Skeleton

---
+++ script.js
@@ -0,0 +1,8 @@
+let adjective = prompt('Enter an adjective');
+let noun = prompt('Enter an noun');
+let verb = prompt('Enter an verb');
+let adverb = prompt('Enter an adverb');
+let story = `Once upon a time, in a ${adjective} ${noun}, a ${verb} ${noun} ${adverb} ${verb}.`
+let madLibOutputDiv = document.getElementById('madLibOutput')
+
+madLibOutputDiv.innerHTML = `<p>${story}</p>`
