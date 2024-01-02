70aa703672fdc1ceb41d6b4c4f7222e1c2847800
s-jaswal <sjasdocs@gmail.com>
2024-01-01 21:10:04 -0500

    did some cool stuff

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let adjective = prompt('Enter an adjective');
 let noun = prompt('Enter an noun');
 let verb = prompt('Enter an verb');
 let adverb = prompt('Enter an adverb');
-let story = `Once upon a time, in a ${adjective} ${noun}, a ${verb} ${noun} ${adverb} ${verb}.`
+let story = `It's the first day of the year so I ${verb} ${noun}, a ${adjective} ${noun} ${verb} ${adverb}.`
 let madLibOutputDiv = document.getElementById('madLibOutput')
 
 madLibOutputDiv.innerHTML = `<p>${story}</p>`
